a2d9422829426ac3d5cbd4e0f8f321f5b7338f12
Giorgi Baramidze <giorgi.baramidze0128@gmail.com>
2023-07-11 13:09:43 +0400

    install whitenoise

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,10 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"), # your static/ files folder
-]
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+   ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
